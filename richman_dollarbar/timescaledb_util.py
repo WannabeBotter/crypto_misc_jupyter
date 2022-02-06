d89372112ca773461a79813e44bd5263a5676816
WannabeBotter <WannabeCryptoBotter@gmail.com>
2022-02-06 16:18:27 +0000

    sync with crypto_trades_downloader

--- richman_dollarbar/timescaledb_util.py
+++ richman_dollarbar/timescaledb_util.py
@@ -4,28 +4,84 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class TimeScaleDBUtil:
+    """
+    TimeScaleDBを使って約定履歴とドルバー情報を保存、読み込むユーティリティクラス    
+    パラメータ
+    ----------
+    user : str, 必須
+        TimeScaleDBのユーザー名。
+    password : str, 必須
+        TimeScaleDBのパスワード。
+    host : str, 必須
+        TimeScaleDBのホスト名。
+    port : str, 必須
+        TimeScaleDBのポート番号。
+    database : str, 必須
+        TimeScaleDBのデータベース名。
+    """
     def __init__(self, user = None, password = None, host = None, port = None, database = None):
-        self._user = user
-        self._password = password
-        self._host = host
-        self._port = port
-        self._database = database
+        if user == None:
+            raise ValueError(f'TimeScaleDBのユーザー名を指定してください')
+        if password == None:
+            raise ValueError(f'TimeScaleDBのパスワードを指定してください')
+        if host == None:
+            raise ValueError(f'TimeScaleDBのホスト名を指定してください')
+        if port == None:
+            raise ValueError(f'TimeScaleDBのポート番号を指定してください')
+        if database == None:
+            raise ValueError(f'TimeScaleDBのデータベース名を指定してください')
         
-        self._sqlalchemy_config = f'postgresql+psycopg2://{self._user}:{self._password}@{self._host}:{self._port}/{self._database}'
-        self._engine = create_engine(self._sqlalchemy_config)
+        _sqlalchemy_config = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+        self._engine = create_engine(_sqlalchemy_config)
         
-        # enum_side型の存在を確認し、なければ作る
+        # enum_side型がデータベース上に存在することを確認し、ない場合は作成する
         _df = self.read_sql_query("SELECT * from pg_type WHERE typname='enum_side'")
         if _df.empty == True:
             self.sql_execute("CREATE TYPE enum_side AS ENUM ('buy', 'sell')")
 
-    def read_sql_query(self, sql = '', index_column = '', dtype={}):
+    def read_sql_query(self, sql = None, index_column = '', dtype={}):
+        """
+        指定されたSQLを実行し、結果をデータフレームで返す関数
+        パラメータ
+        ----------
+        sql : str, 必須
+            実行するSQL文。
+        index_column : str, default = ''
+            出力するデータフレームのインデックスにする列名。
+        dtype : dict, default = {}
+
+        返り値
+        -------
+        df : pandas.DataFrame
+            SQLクエリ結果を含んだデータフレーム。
+        """
+        if sql == None:
+            raise ValueError(f'実行するSQL文を指定してください')
+        if hasattr(self, '_engine') == False:
+            raise UnboundLocalError('SQLAlchemyが初期化されていません')
+        
         df = pd.read_sql_query(sql, self._engine, dtype=dtype)
         if len(index_column) > 0:
             df = df.set_index(index_column)
         return df
     
-    def sql_execute(self, sql = ''):
+    def sql_execute(self, sql = None):
+        """
+        指定されたSQLを実行し、結果をlistで返す関数
+        パラメータ
+        ----------
+        sql : str, 必須
+            実行するSQL文。
+
+        返り値
+        -------
+        SQLクエリ結果を含んだdict。
+        """
+        if sql == None:
+            raise ValueError(f'実行するSQL文を指定してください')
+        if hasattr(self, '_engine') == False:
+            raise UnboundLocalError('SQLAlchemyが初期化されていません')
+        
         return self._engine.execute(sql)
     
     def df_to_sql(self, df = None, schema = None, if_exists = 'fail'):
@@ -46,7 +102,7 @@ class TimeScaleDBUtil:
         
         # トレード記録テーブルを作成
         _sql = (f'DROP TABLE IF EXISTS "{_table_name}" CASCADE;'
-                f' CREATE TABLE IF NOT EXISTS "{_table_name}" (datetime TIMESTAMP WITH TIME ZONE NOT NULL, id text, side enum_side NOT NULL, liquidation BOOL NOT NULL, price NUMERIC NOT NULL, amount NUMERIC NOT NULL, dollar NUMERIC NOT NULL, dollar_cumsum NUMERIC NOT NULL, UNIQUE(datetime, id));'
+                f' CREATE TABLE IF NOT EXISTS "{_table_name}" (datetime TIMESTAMP WITH TIME ZONE NOT NULL, id text, side enum_side NOT NULL, liquidation BOOL NOT NULL, price NUMERIC NOT NULL, amount NUMERIC NOT NULL, dollar NUMERIC NOT NULL, dollar_cumsum NUMERIC NOT NULL, buy_dollar_cumsum NUMERIC NOT NULL, sell_dollar_cumsum NUMERIC NOT NULL, UNIQUE(datetime, id));'
                 f' CREATE INDEX ON "{_table_name}" (datetime DESC);'
                 f' CREATE INDEX ON "{_table_name}" (datetime DESC, dollar_cumsum);'
                 f" SELECT create_hypertable ('{_table_name}', 'datetime');")
@@ -59,13 +115,15 @@ class TimeScaleDBUtil:
         if _df.empty == True:
             return None
         
-        _df = self.read_sql_query(f'SELECT * FROM "{_table_name}" ORDER BY dollar_cumsum DESC LIMIT 1', dtype={'price': str, 'amount': str, 'dollar': str, 'dollar_cumsum': str})
+        _df = self.read_sql_query(f'WITH time_filtered AS (SELECT * FROM "{_table_name}" ORDER BY datetime DESC LIMIT 1000) SELECT * FROM time_filtered ORDER BY dollar_cumsum DESC LIMIT 1', dtype={'price': str, 'amount': str, 'dollar': str, 'dollar_cumsum': str, 'buy_dollar_cumsum': str, 'sell_dollar_cumsum': str})
         if len(_df) > 0:
             _to_decimal = lambda x: Decimal(x)
             _df['price'] = _df['price'].apply(_to_decimal)
             _df['amount'] = _df['amount'].apply(_to_decimal)
             _df['dollar'] = _df['dollar'].apply(_to_decimal)
             _df['dollar_cumsum'] = _df['dollar_cumsum'].apply(_to_decimal)
+            _df['buy_dollar_cumsum'] = _df['buy_dollar_cumsum'].apply(_to_decimal)
+            _df['sell_dollar_cumsum'] = _df['sell_dollar_cumsum'].apply(_to_decimal)
             return _df.iloc[0]
         
         return None
@@ -77,13 +135,15 @@ class TimeScaleDBUtil:
         if _df.empty == True:
             return None
         
-        _df = self.read_sql_query(f'SELECT * FROM "{_table_name}" ORDER BY dollar_cumsum ASC LIMIT 1', dtype={'price': str, 'amount': str, 'dollar': str, 'dollar_cumsum': str})
+        _df = self.read_sql_query(f'WITH time_filtered AS (SELECT * FROM "{_table_name}" ORDER BY datetime ASC LIMIT 1000) SELECT * FROM time_filtered ORDER BY dollar_cumsum ASC LIMIT 1', dtype={'price': str, 'amount': str, 'dollar': str, 'dollar_cumsum': str})
         if len(_df) > 0:
             _to_decimal = lambda x: Decimal(x)
             _df['price'] = _df['price'].apply(_to_decimal)
             _df['amount'] = _df['amount'].apply(_to_decimal)
             _df['dollar'] = _df['dollar'].apply(_to_decimal)
             _df['dollar_cumsum'] = _df['dollar_cumsum'].apply(_to_decimal)
+            _df['buy_dollar_cumsum'] = _df['buy_dollar_cumsum'].apply(_to_decimal)
+            _df['sell_dollar_cumsum'] = _df['sell_dollar_cumsum'].apply(_to_decimal)
             return _df.iloc[0]
         
         return None
@@ -101,20 +161,20 @@ class TimeScaleDBUtil:
         
         # ドルバー記録テーブルを作成
         _sql = (f'DROP TABLE IF EXISTS "{_table_name}" CASCADE;'
-                f' CREATE TABLE IF NOT EXISTS "{_table_name}" (datetime TIMESTAMP WITH TIME ZONE NOT NULL, datetime_from TIMESTAMP WITH TIME ZONE NOT NULL, id text, id_from text, open NUMERIC NOT NULL, high NUMERIC NOT NULL, low NUMERIC NOT NULL, close NUMERIC NOT NULL, amount NUMERIC NOT NULL, dollar_volume NUMERIC NOT NULL, dollar_buy_volume NUMERIC NOT NULL, dollar_sell_volume NUMERIC NOT NULL, dollar_liquidation_buy_volume NUMERIC NOT NULL, dollar_liquidation_sell_volume NUMERIC NOT NULL, dollar_cumsum NUMERIC NOT NULL, UNIQUE(datetime, id));'
+                f' CREATE TABLE IF NOT EXISTS "{_table_name}" (datetime TIMESTAMP WITH TIME ZONE NOT NULL, datetime_from TIMESTAMP WITH TIME ZONE NOT NULL, id text, id_from text, open NUMERIC NOT NULL, high NUMERIC NOT NULL, low NUMERIC NOT NULL, close NUMERIC NOT NULL, amount NUMERIC NOT NULL, dollar_volume NUMERIC NOT NULL, dollar_buy_volume NUMERIC NOT NULL, dollar_sell_volume NUMERIC NOT NULL, dollar_liquidation_buy_volume NUMERIC NOT NULL, dollar_liquidation_sell_volume NUMERIC NOT NULL, dollar_cumsum NUMERIC NOT NULL, buy_dollar_cumsum NUMERIC NOT NULL, sell_dollar_cumsum NUMERIC NOT NULL, UNIQUE(datetime, id));'
                 f' CREATE INDEX ON "{_table_name}" (datetime DESC);'
                 f' CREATE INDEX ON "{_table_name}" (datetime DESC, dollar_cumsum);'
                 f" SELECT create_hypertable ('{_table_name}', 'datetime');")
         self.sql_execute(_sql)
         
-    def get_latest_dollarbar(self, exchange='ftx', symbol='BTC-PERP', interval=10_000_000):
+    def get_latest_dollarbar(self, exchange='ftx', symbol='BTC-PERP', interval=5_000_000):
         _table_name = self.get_dollarbar_table_name(exchange, symbol, interval)
         
         _df = self.read_sql_query(f"select * from information_schema.tables where table_name='{_table_name}'")
         if _df.empty == True:
             return None
         
-        _df = self.read_sql_query(f'SELECT * FROM "{_table_name}" ORDER BY datetime DESC, id DESC LIMIT 1', dtype={'open': str, 'high': str, 'low': str, 'close': str, 'amount': str, 'dollar_volume': str, 'dollar_buy_volume': str, 'dollar_sell_volume': str, 'dollar_cumsum': str})
+        _df = self.read_sql_query(f'SELECT * FROM "{_table_name}" ORDER BY datetime DESC, id DESC LIMIT 1', dtype={'open': str, 'high': str, 'low': str, 'close': str, 'amount': str, 'dollar_volume': str, 'dollar_buy_volume': str, 'dollar_sell_volume': str, 'dollar_liquidation_buy_volume': str, 'dollar_liquidation_sell_volume': str, 'dollar_cumsum': str})
         if len(_df) > 0:
             _to_decimal = lambda x: Decimal(x)
             _df['open'] = _df['open'].apply(_to_decimal)
@@ -128,6 +188,16 @@ class TimeScaleDBUtil:
             _df['dollar_liquidation_buy_volume'] = _df['dollar_liquidation_buy_volume'].apply(_to_decimal)
             _df['dollar_liquidation_sell_volume'] = _df['dollar_liquidation_sell_volume'].apply(_to_decimal)
             _df['dollar_cumsum'] = _df['dollar_cumsum'].apply(_to_decimal)
+            _df['buy_dollar_cumsum'] = _df['buy_dollar_cumsum'].apply(_to_decimal)
+            _df['sell_dollar_cumsum'] = _df['sell_dollar_cumsum'].apply(_to_decimal)
             return _df.iloc[0]
         
         return None
+
+    def load_dollarbars(exchange='ftx', symbol='BTC-PERP', interval=5_000_000, from_str, to_str):
+        _table_name = self.get_dollarbar_table_name(exchange, symbol, interval)
+
+        _sql = f"SELECT * FROM \"{table_name}\" WHERE datetime >= '{from_str}' AND datetime < '{to_str}' ORDER BY dollar_cumsum ASC"
+        _df = _dbutil.read_sql_query(sql = _sql)
+        _df = _df[['datetime', 'open', 'high', 'low', 'close', 'dollar_volume', 'dollar_buy_volume', 'dollar_sell_volume', 'dollar_liquidation_buy_volume', 'dollar_liquidation_sell_volume', 'dollar_cumsum', 'dollar_buy_cumsum', 'dollar_sell_cumsum']]
+        return df
